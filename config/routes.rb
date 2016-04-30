aa477a6a9568decea53afc827d56732bedac5d27
yokrh <y.oka.gml@gmail.com>
2016-04-30 05:50:49 +0000

    add index in home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #
+    root 'home#index'
+    get '/home' => 'home#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
