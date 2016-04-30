63ab4df79c065f3ba16c672102cbc47fbcca5704
yokrh <y.oka.gml@gmail.com>
2016-04-30 12:15:49 +0000

    add callback in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   #
     root 'home#index'
     get '/home' => 'home#index'
-
+  #
+    get '/auth/:provider/callback' => 'sessions#callback'
+    post '/auth/:provider/callback' => 'sessions#callback'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
