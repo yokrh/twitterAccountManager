30e5ff459376e9804048e196f61c17a5b24b7bd0
yokrh <y.oka.gml@gmail.com>
2016-04-30 14:13:20 +0000

    add access token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430060244) do
+ActiveRecord::Schema.define(version: 20160430140324) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
     t.string   "nickname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "access_token_key"
+    t.string   "access_token_secret"
   end
 
 end
