b4e77bd0d239038309646587a5c3ee838a244c80
yokrh <y.oka.gml@gmail.com>
2016-05-02 06:29:33 +0000

    add User#add/delete_access_token_data

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ActiveRecord::Base
       user.nickname = auth['info']['nickname']
     end
   end
+  
+  def add_access_token_data(auth)
+    self.access_token_key = auth['extra']['access_token'].params[:oauth_token]
+    self.access_token_secret = auth['extra']['access_token'].params[:oauth_token_secret]
+    self.save
+  end
+  
+  def delete_access_token_data
+    self.access_token_key = nil
+    self.access_token_secret = nil
+    self.save
+  end
 end
