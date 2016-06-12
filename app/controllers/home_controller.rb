f30b0bd6bbc85b8e1512e3b3f66fa2dcf8da2bd1
yokrh <y.oka.gml@gmail.com>
2016-06-12 06:51:05 +0000

    modify search about getting datas as possible

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,12 @@ class HomeController < ApplicationController
 
   # 検索の絞り込み
   def index_ajax
-    index # @favosが空になっているので。何かうまい方法ないのかな。dbに@xxx入れとく？
+    return unless current_user
+
+    fetch_user_favos_all if params[:object] == 'favos'
+    fetch_user_favorares_all if params[:object] == 'favorares'
+    #fetch_user_rts_all if params[:object] == 'rts'
+    #fetch_user_rtrares_all if params[:object] == 'rtrares'
 
     unless params[:username].nil? || params[:username] == ""
       eval("@#{params[:object]}").select!{ |o| o.user.name == params[:username] }
@@ -65,5 +70,49 @@ class HomeController < ApplicationController
     @rts_by_me = client.retweeted_by_me count: count
     @rts = @rts_by_me.map{ |r| r.retweeted_status }
     @rtrares = client.retweets_of_me count:count
+  end
+
+  # 可能な限りのfetch
+  # favo
+  def fetch_user_favos_all
+    count = 200
+    @favos = client.favorites(count: count)
+    20.times do
+      max_id = @favos.last.id 
+      res = client.favorites(max_id: max_id, count: count)
+      @favos.concat(res).uniq!
+    end
+  end
+  # favorare
+  def fetch_user_favorares_all
+    count = 200
+    @tweets = client.user_timeline(count: count)
+    20.times do
+      max_id = @tweets.last.id unless @tweets.empty?
+      res = client.user_timeline(max_id: max_id, count: count)
+      @tweets.concat(res).uniq!
+    end
+    @favorares = @tweets.select{ |t| t.favorite_count > 0 }
+  end
+  # rt
+  def fetch_user_rts_all
+    count = 200
+    @rts_by_me = client.retweeted_by_me(count: count)
+    20.times do
+      max_id = @rts_by_me.last.id unless @rts_by_me.empty?
+      res = client.retweeted_by_me(max_id: max_id, count: count)
+      @rts_by_me.concat(res).uniq!
+    end
+    @rts = @rts_by_me.map{ |r| r.retweeted_status }
+  end
+  # rts
+  def fetch_user_rtrares_all
+    count = 200
+    @rtrares = client.retweets_of_me(count: count)
+    20.times do
+      max_id = @rtrares.last.id unless @rtrares.empty?
+      res = client.retweets_of_me(max_id: max_id, count: count)
+      @rtrares.concat(res).uniq!
+    end
   end
 end
